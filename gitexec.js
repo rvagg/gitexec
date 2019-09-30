16886c5c32d0ca2c6e9a930f9954c6ac8c5fc483
XhmikosR <xhmikosr@gmail.com>
2019-09-30 10:18:09 +0300

    Add linting with standard.

--- gitexec.js
+++ gitexec.js
@@ -1,25 +1,21 @@
 'use strict'
 
-const spawn    = require('child_process').spawn
-    , bl       = require('bl')
-
+const spawn = require('child_process').spawn
+const bl = require('bl')
 
 function exec (repoPath, gitcmd) {
-  const child = spawn('bash', [ '-c', gitcmd ], { env: process.env, cwd: repoPath })
+  const child = spawn('bash', ['-c', gitcmd], { env: process.env, cwd: repoPath })
 
   child.stderr.pipe(bl((err, _data) => {
-    if (err)
-      return child.stdout.emit('error', err)
+    if (err) { return child.stdout.emit('error', err) }
 
-    if (_data.length)
-      process.stderr.write(_data)
+    if (_data.length) { process.stderr.write(_data) }
   }))
 
   child.on('close', (code) => {
-    if (!code)
-      return
+    if (!code) { return }
     child.stdout.emit(
-        'error'
+      'error'
       , new Error('git command [' + gitcmd + '] exited with code ' + code)
     )
   })
@@ -27,16 +23,13 @@ function exec (repoPath, gitcmd) {
   return child.stdout
 }
 
-
 function execCollect (repoPath, gitcmd, callback) {
   exec(repoPath, gitcmd).pipe(bl((err, _data) => {
-    if (err)
-      return callback(err)
+    if (err) { return callback(err) }
 
     callback(null, _data.toString())
   }))
 }
 
-
-module.exports.exec        = exec
+module.exports.exec = exec
 module.exports.execCollect = execCollect
